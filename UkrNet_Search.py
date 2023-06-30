560b155beed67a9d44e62a736d8641126459a566
Игорь Лыхолит <106541658+IgrLi@users.noreply.github.com>
2023-06-30 17:56:32 +0300

    Add files via upload

---
+++ UkrNet_Search.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+options.add_argument("start-maximized")
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service('"D:\chromedriver_win32\chromedriver.exe'), options=options)
+driver.get("https://www.ukr.net/")
+time.sleep(2)
+input_element = driver.find_element(By.XPATH, '//*[@id="search-input"]')
+input_element.send_keys("новини")
+time.sleep(2)
+driver.close()
